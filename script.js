c1c1e6c1f0b2fff05d0b7662998f2cce2a75234e
arbiva <arbiva@post.bgu.ac.il>
2021-01-28 23:20:44 +0200

    fix coad

--- script.js
+++ script.js
@@ -1,19 +1,9 @@
 const nextBtn = document.querySelector('#next'),
       addBtn = document.querySelector('#add'),
-      imgDiv = document.querySelector("#photoholder"),
-      img = document.querySelector("#imgAddress");
+      heroCardtext = document.querySelector(".card-text"),
+      heroCardImg = document.querySelector(".card-img-top"),
+      heroContainer = document.querySelector(".container");
 const startIndex = 0;
-
-window.onload = function() {
-    addBtn.addEventListener("click", function(e){
-        e.preventDefault();
-        addNewHero();
-    })
-}
-
-function addNewHero(){
-    location.herf = "index2.html";
-}
 
 
 class Hero {
@@ -24,23 +14,59 @@ class Hero {
     }   
 }
 
-const hero = [
-    new hero(Storm, [WetherControl, flying], url="https://s-media-cache-ak0.pinimg.com/736x/63/f1/aa/63f1aaadcbf4941d347d9e672b444f59.jpg"),
-    new hero(BlackPanther,[EnhancedStrength, Speed], url="https://upload.wikimedia.org/wikipedia/en/9/9f/Black_Panther_OS_Vol_1_2.png" ),
-    new hero(CaptainAmerica,[SuperSoldier, CombatSkills], url="https://www.artmajeur.com/medias/standard/b/e/benny-arte/artwork/12974891_capitain-america.jpg"),
-    new hero(Batman,[advancedTechnologies, Armor], url='https://reel360.com/wp-content/uploads/2020/09/Screen-Shot-2020-09-29-at-8.22.27-AM.jpg'),
-    new hero(Spiderman,[WebMaking, Jumping], url="https://cdn.vox-cdn.com/thumbor/K5IUSjv1ArPfjh_IZUp4DPsoZMY=/1400x1400/filters:format(png)/cdn.vox-cdn.com/uploads/chorus_asset/file/22024938/marvels_spiderman_remastered_screenshot_01_disclaimer_en_01oct20.png"),
+const heroes = [
+    new Hero('Storm', ['WetherControl', 'flying'], "https://s-media-cache-ak0.pinimg.com/736x/63/f1/aa/63f1aaadcbf4941d347d9e672b444f59.jpg"),
+    new Hero('BlackPanther',['EnhancedStrength', 'Speed'], "https://upload.wikimedia.org/wikipedia/en/9/9f/Black_Panther_OS_Vol_1_2.png" ),
+    new Hero('CaptainAmerica',['SuperSoldier', 'CombatSkills'], "https://www.artmajeur.com/medias/standard/b/e/benny-arte/artwork/12974891_capitain-america.jpg"),
+    new Hero('Batman',['advancedTechnologies', 'Armor'], 'https://reel360.com/wp-content/uploads/2020/09/Screen-Shot-2020-09-29-at-8.22.27-AM.jpg'),
+    new Hero('Spiderman',['WebMaking', 'Jumping'], "https://cdn.vox-cdn.com/thumbor/K5IUSjv1ArPfjh_IZUp4DPsoZMY=/1400x1400/filters:format(png)/cdn.vox-cdn.com/uploads/chorus_asset/file/22024938/marvels_spiderman_remastered_screenshot_01_disclaimer_en_01oct20.png"),
 ];
-const heroToDisplay = [];
+let currentHeroes = localStorage.getItem('currentHeroes')
+currentHeroes = JSON.parse(currentHeroes)
+let stringArray = JSON.stringify(heroes) //only than the storage gets the data - complex data requires stringify
+localStorage.setItem('currentHeroes', stringArray)
 
-
-
-function updateHedroesCard() {
+function insertHeroesToDisplay(event) {
+    let counter = 0;
     for(const hero of heroes) {
-        heroCard.innerHTML += `<card>
-        <div scope="card">${hero.name}</div>
-        <div>${hero.powers['${value}']}</div>
-        <div>${hero.img}</div>
-      </card>`;
+        let row = document.createElement('div')
+        row.classList.add('row')
+        for(i=0; i< 3 && i<heroes.length && counter< heroes.length; i++, counter++)
+        {row.insertAdjacentHTML("beforeend", `
+        <div class="col-6 col-md-4"><div class="card" style="width: 18rem;">
+        <img src= "${heroes[counter].img}" class="card-img-top"/>
+        <div class="card-body">
+          <p class="card-text"> ${heroes[counter].name} ${heroes[counter].powers}</p>
+      </div>
+        `)}
+        heroContainer.appendChild(row)};
     }
+
+insertHeroesToDisplay();
+
+
+    nextBtn.addEventListener('click', function () {
+        startIndex = startIndex + 6 > imgArr.length -1 ? startIndex : startIndex + 6;
+        render();
+    });
+
+
+//getting Galits' file
+window.onload = function() {
+    addBtn.addEventListener("click", function(e){
+        e.preventDefault();
+        addNewHero();
+    })
 }
+
+function addNewHero(){
+    location.href = "index2.html";
+}
+
+// let newHeroString = localStorage.getItem('Hero');
+
+// newHero = JSON.parse(newHeroString);
+
+// let newHerosString = localStorage.getItem('Heros');
+
+// let heros = JSON.parse(newHerosString);
